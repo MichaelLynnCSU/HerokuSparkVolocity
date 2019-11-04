8fcc945e42c0c6af6e21901f0109fbe28914af5b
Michael Lynn <hailviral101@yahoo.com>
2019-11-03 18:12:45 -0700

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import static spark.Spark.*;
 public class Main {
 
     public static void main(String[] args) {
+        staticFileLocation("/public");
         port(getHerokuAssignedPort());
         new LayoutController();
     }
